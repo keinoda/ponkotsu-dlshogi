7401b7c07ae7baf54919a83929bd2b24fa92ca96
JJ1GUJ <jj1guj@jj1guj.net>
2023-02-03 21:56:56 +0900

    first commit

---
+++ policy_value_network_densenet.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from dlshogi.common import *
+
+class Bias(nn.Module):
+    def __init__(self, shape):
+        super(Bias, self).__init__()
+        self.bias=nn.Parameter(torch.zeros(shape))
+
+    def forward(self, input):
+        return input + self.bias
+
+class DenseLayer(nn.Module):
+    def __init__(self, channels, growth_rate):
+        super(DenseLayer, self).__init__()
+        self.norm1=nn.BatchNorm2d(channels)
+        self.relu1=nn.ReLU(inplace=True)
+        # self.conv1=nn.Conv2d(channels,growth_rate*4,kernel_size=1,bias=False)
+        self.conv1=nn.Conv2d(channels,growth_rate,kernel_size=1,bias=False)
+        # self.norm2=nn.BatchNorm2d(growth_rate*4)
+        self.norm2=nn.BatchNorm2d(growth_rate)
+        self.relu2=nn.ReLU(inplace=True)
+        # self.conv2=nn.Conv2d(growth_rate*4,growth_rate,kernel_size=3,padding=1,bias=False)
+        self.conv2=nn.Conv2d(growth_rate,growth_rate,kernel_size=3,padding=1,bias=False)
+        # self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        # self.bn1 = nn.BatchNorm2d(channels)
+        # self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        # self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        out=torch.cat(x,1)
+        out=self.norm1(out)
+        out=self.relu1(out)
+        out=self.conv1(out)
+        
+        out=self.norm2(out)
+        out=self.relu2(out)
+        out=self.conv2(out)
+        # out = self.conv1(x)
+        # out = self.bn1(out)
+        # out = F.relu(out)
+
+        # out = self.conv2(out)
+        # out = self.bn2(out)
+
+        # return F.relu(out)
+        return out
+    
+class DenseBlock(nn.ModuleDict):
+    def __init__(self,num_layers,channels,growth_rate):
+        super(DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer=DenseLayer(channels=channels+i*growth_rate,
+                             growth_rate=growth_rate)
+            self.add_module(f"denselayer{i+1}",layer)
+    def forward(self,x0):
+        x=[x0]
+        for name,layer in self.items():
+            out=layer(x)
+            x.append(out)
+        return torch.cat(x,1)
+
+class TransitionLayer(nn.Sequential):
+    def __init__(self,in_channels,out_channels):
+        super().__init__()
+        self.add_module("norm",nn.BatchNorm2d(in_channels))
+        self.add_module("relu",nn.ReLU(inplace=True))
+        self.add_module("conv",nn.Conv2d(in_channels,out_channels,kernel_size=1,bias=False))
+
+class PolicyValueNetwork(nn.Module):
+    def __init__(self, growth_rate=32, blocks=(10,), channels=192, fcl=256):
+        super(PolicyValueNetwork, self).__init__()
+        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=3, padding=1, bias=False)
+        self.conv1_1_2 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=1, padding=0, bias=False)
+        self.conv1_2 = nn.Conv2d(in_channels=FEATURES2_NUM, out_channels=channels, kernel_size=1, bias=False)
+        self.norm1 = nn.BatchNorm2d(channels)
+
+        # Dense Block及びTransition Layerを作成
+        self.blocks=nn.Sequential()
+        for i,num_layers in enumerate(blocks):
+            block=DenseBlock(
+                num_layers=num_layers,
+                channels=channels,
+                growth_rate=growth_rate
+            )
+            self.blocks.add_module(f"denseblock{i+1}",block)
+
+            channels=channels+num_layers*growth_rate
+            if i!=len(blocks)-1:
+                # 最後のDense Block出ない場合はTransition Layerを追加
+                trans=TransitionLayer(in_channels=channels,out_channels=channels//2)
+                self.blocks.add_module(f"transition{i+1}",trans)
+                channels//=2
+        
+
+        # policy head
+        self.policy_conv = nn.Conv2d(in_channels=channels, out_channels=MAX_MOVE_LABEL_NUM, kernel_size=1, bias=False)
+        self.policy_bias = Bias(9*9*MAX_MOVE_LABEL_NUM)
+
+        # value head
+        self.value_conv1 = nn.Conv2d(in_channels=channels, out_channels=MAX_MOVE_LABEL_NUM, kernel_size=1, bias=False)
+        self.value_norm1 = nn.BatchNorm2d(MAX_MOVE_LABEL_NUM)
+        self.value_fc1 = nn.Linear(9*9*MAX_MOVE_LABEL_NUM, fcl)
+        self.value_fc2 = nn.Linear(fcl, 1)
+
+    def forward(self, x1, x2):
+        x_1_1 = self.conv1_1_1(x1)
+        x_1_2 = self.conv1_1_2(x1)
+        x_2 = self.conv1_2(x2)
+        x = F.relu(self.norm1(x_1_1 + x_1_2 + x_2))
+
+        # resnet blocks
+        x = self.blocks(x)
+
+        # policy head
+        policy = self.policy_conv(x)
+        policy = self.policy_bias(torch.flatten(policy, 1))
+
+        # value head
+        value = F.relu(self.value_norm1(self.value_conv1(x)))
+        value = F.relu(self.value_fc1(torch.flatten(value, 1)))
+        value = self.value_fc2(value)
+
+        return policy, value
---
+++ train.sh
@@ -0,0 +1,51 @@
+last=1
+
+# 変数設定
+name="densenet10"
+model_dir="/mnt/d/densenet/model"
+checkpoint_dir="/mnt/d/densenet"
+data_dir="/mnt/d/ShogiAIBookData"
+
+# 最新のチェックポイント+1から学習を再開する
+for i in $(ls -v ${model_dir}/checkpoint_${name}-???.pth 2>/dev/null); do chkp=$i;
+    done
+
+if [ -v chkp ];then
+    start=$(expr ${chlp: -7:3} + 1)
+else
+    start=1
+fi
+
+for ((i=$start; i<=$last; i++)); do
+    iii=$(printf "%03d" $i)
+    jjj=$(printf "%03d" $((i-1)))
+    kkk=$(printf "%03d" $(((i-1) % 24 +1)))
+    src="${data_dir}/floodgate_2019-2021_r3500-${kkk}.hcpe ${data_dir}/suisho3kai-${kkk}.hcpe ${data_dir}/dlshogi_with_gct-${kkk}.hcpe"
+
+    # チェックポイントが存在する場合、最新のチェックポイントから学習を継続
+    if [ $i -eq 1 ]; then
+        resume=""
+    else
+        resume="-r ${checkpoint_dir}/checkpoint_${name}-${jjj}.pth"
+    fi
+
+    # モデルのファイル名
+    model="${model_dir}/model_${name}-{epoch:03}.pth"
+
+    # チェックポイントのファイル名
+    checkpoint="${checkpoint_dir}/checkpoint_${name}-{epoch:03}.pth"
+
+    echo epoch ${i} start
+
+    # 学習
+    # python -m pydlshogi2.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
+    #  ${resume} --checkpoint ${checkpoint} -g 0 -b 3000 -e 1
+    python -m dlshogi.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
+     ${resume} --checkpoint ${checkpoint} --network policy_value_network_densenet.PolicyValueNetwork --model ${model} -e 1\
+     --eval_interval 100
+
+    
+    if [ $? -ne 0 ]; then
+        break
+    fi
+done
