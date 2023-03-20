8559d085a4b305fe14c8f48328b288f9bcb90885
JJ1GUJ <jj1guj@jj1guj.net>
2023-03-20 21:14:59 +0900

    GCTの学習データでの追加学習を行う
    selfplay_gct-???.hcpe3を使用する

---
+++ train_gct.sh
@@ -0,0 +1,57 @@
+last=275
+
+# 変数設定
+save_dir=$1
+name="densenet10_g32_c192_add_options_kernel3_adding_gct"
+checkpoint_dir="${save_dir}/${name}"
+model_dir="${save_dir}/${name}/model"
+log_dir="${save_dir}/${name}"
+data_dir=$2
+
+# 最新のチェックポイント+1から学習を再開する
+for i in $(ls -v ${checkpoint_dir}/checkpoint_${name}-???.pth 2>/dev/null); do chkp=$i;
+    done
+
+if [ -v chkp ];then
+    start=$(expr ${chkp: -7:3} + 1)
+else
+    start=251
+fi
+
+for ((i=$start; i<=$last; i++)); do
+    iii=$(printf "%03d" $(((i-251)*3+1)))
+    jjj=$(printf "%03d" $(((i-251)*3+2)))
+    kkk=$(printf "%03d" $(((i-251)*3+3)))
+    rrr=$(printf "%03d" $((3*i-503)))
+    src="${data_dir}/selfplay_gct-${iii}.hcpe3 ${data_dir}/selfplay_gct-${jjj}.hcpe3 ${data_dir}/selfplay_gct-${kkk}.hcpe3"
+
+    if [ $i -eq $last ];then
+        src="${src} ${data_dir}/nyugyoku"
+    fi
+
+    # チェックポイントが存在する場合、最新のチェックポイントから学習を継続
+    if [ $i -eq 251 ]; then
+        resume="-r ${checkpoint_dir}/checkpoint_densenet10_g32_c192_add_options_kernel3_adding_no-reset-250.pth"
+    else
+        resume="-r ${checkpoint_dir}/checkpoint_${name}-${rrr}.pth"
+    fi
+
+    # モデルのファイル名
+    model="${model_dir}/model_${name}-{epoch:03}.pth"
+
+    # チェックポイントのファイル名
+    checkpoint="${checkpoint_dir}/checkpoint_${name}-{epoch:03}.pth"
+
+    echo epoch $((3*i-503))-$((3*i-501)) start
+
+    # 学習
+    python -m dlshogi.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
+     ${resume} --checkpoint ${checkpoint} --network policy_value_network_densenet.PolicyValueNetwork --model ${model} -e 3\
+    --use_average --use_evalfix --use_swa --use_amp --temperature 1 --reset_scheduler --lr 0.0002\
+    --lr_scheduler MultiStepLR'('milestones=[260,280],gamma=0.1')' --log ${log_dir}/train_log.txt
+
+    
+    if [ $? -ne 0 ]; then
+        break
+    fi
+done
