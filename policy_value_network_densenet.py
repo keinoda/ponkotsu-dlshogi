ff32ca3d8b7cecc7ba1675a9f76deac6db430203
JJ1GUJ <jj1guj@jj1guj.net>
2023-03-24 00:33:55 +0900

    入力層におけるモデル構造の改良
    CNNをかけた後の出力の要素和を取っていたところをチャンネル方向に結合するようにした

--- policy_value_network_densenet.py
+++ policy_value_network_densenet.py
@@ -55,11 +55,12 @@ class TransitionLayer(nn.Sequential):
         self.add_module("conv",nn.Conv2d(in_channels,out_channels,kernel_size=1,bias=False))
 
 class PolicyValueNetwork(nn.Module):
-    def __init__(self, growth_rate=32, blocks=(10,), channels=192, fcl=256):
+    def __init__(self, growth_rate=32, blocks=(10,), fcl=256):
         super(PolicyValueNetwork, self).__init__()
-        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=3, padding=1, bias=False)
-        self.conv1_1_2 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=1, padding=0, bias=False)
-        self.conv1_2 = nn.Conv2d(in_channels=FEATURES2_NUM, out_channels=channels, kernel_size=1, bias=False)
+        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=growth_rate, kernel_size=3, padding=1, bias=False)
+        self.conv1_1_2 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=growth_rate, kernel_size=1, padding=0, bias=False)
+        self.conv1_2 = nn.Conv2d(in_channels=FEATURES2_NUM, out_channels=growth_rate, kernel_size=1, bias=False)
+        channels=growth_rate*3+FEATURES1_NUM+FEATURES2_NUM
         self.norm1 = nn.BatchNorm2d(channels)
 
         # Dense Block及びTransition Layerを作成
@@ -93,10 +94,12 @@ class PolicyValueNetwork(nn.Module):
     def forward(self, x1, x2):
         x_1_1 = self.conv1_1_1(x1)
         x_1_2 = self.conv1_1_2(x1)
-        x_2 = self.conv1_2(x2)
-        x = F.relu(self.norm1(x_1_1 + x_1_2 + x_2))
+        x_2_1 = self.conv1_2(x2)
+        x = [x1, x_1_1, x_1_2, x2, x_2_1]
+        x = torch.cat(x, 1)
+        x = F.relu(self.norm1(x))
 
-        # resnet blocks
+        # dense blocks
         x = self.blocks(x)
 
         # policy head
