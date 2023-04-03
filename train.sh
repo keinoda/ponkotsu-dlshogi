5015d39b7318fe49911bb48a9c5de0cf96f00484
jj1guj <jj1guj@jj1guj.net>
2023-04-03 22:25:59 +0900

    入力部のチャンネル数を変更
    あまり精度は上がらなかった

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ last=144
 
 # 変数設定
 save_dir=$1
-name="densenet10_g32_cat_input_dlshogi"
+name="densenet10_g32_cat_input_dlshogi_c192"
 checkpoint_dir="${save_dir}/${name}"
 model_dir="${save_dir}/${name}/model"
 log_dir="${save_dir}/${name}"
