f1e9ae2de32a270a8773e4c30dd7cb489efe9ff0
jj1guj <jj1guj@jj1guj.net>
2023-11-01 20:39:26 +0900

    padding=1だった部分を0に書き換え

--- policy_value_network_densenet.py
+++ policy_value_network_densenet.py
@@ -17,10 +17,10 @@ class DenseLayer(nn.Module):
         super(DenseLayer, self).__init__()
         self.norm1=nn.BatchNorm2d(channels)
         self.relu1=nn.ReLU(inplace=True)
-        self.conv1=nn.Conv2d(channels,growth_rate*4,kernel_size=3,padding=1,bias=False)
+        self.conv1=nn.Conv2d(channels,growth_rate*4,kernel_size=3,padding=0,bias=False)
         self.norm2=nn.BatchNorm2d(growth_rate*4)
         self.relu2=nn.ReLU(inplace=True)
-        self.conv2=nn.Conv2d(growth_rate*4,growth_rate,kernel_size=3,padding=1,bias=False)
+        self.conv2=nn.Conv2d(growth_rate*4,growth_rate,kernel_size=3,padding=0,bias=False)
 
     def forward(self, x):
         out=torch.cat(x,1)
@@ -57,7 +57,7 @@ class TransitionLayer(nn.Sequential):
 class PolicyValueNetwork(nn.Module):
     def __init__(self, growth_rate=32, blocks=(10,), channels=192, fcl=256):
         super(PolicyValueNetwork, self).__init__()
-        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=3, padding=1, bias=False)
+        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=3, padding=0, bias=False)
         self.conv1_1_2 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=channels, kernel_size=1, padding=0, bias=False)
         self.conv1_2 = nn.Conv2d(in_channels=FEATURES2_NUM, out_channels=channels, kernel_size=1, bias=False)
         self.norm1 = nn.BatchNorm2d(channels)
