115c0a90189ebc14254396cb20846724e940eda5
jj1guj <jj1guj@jj1guj.net>
2024-03-10 21:35:14 +0900

    ファイルへのログ出力をコメントアウトしていたため修正

--- train.sh
+++ train.sh
@@ -43,7 +43,7 @@ for ((i=$start; i<=$last; i++)); do
     python -m dlshogi.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
      ${resume} --checkpoint ${checkpoint} --network policy_value_network_densenet.PolicyValueNetwork --model ${model} -e 1\
     --use_average --use_evalfix --use_swa --use_amp --temperature 0 --lr 0.2\
-    --lr_scheduler ReduceLROnPlateau'('factor=0.5')' #--log ${log_dir}/train_log.txt
+    --lr_scheduler ReduceLROnPlateau'('factor=0.5')' --log ${log_dir}/train_log.txt
 
     
     if [ $? -ne 0 ]; then
