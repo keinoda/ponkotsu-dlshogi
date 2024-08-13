158157f76e72a1f65d7b2744eafa32e11c6dc744
jj1guj <jj1guj@jj1guj.net>
2024-08-14 06:45:06 +0900

    SWAを無効化

--- train_prior.sh
+++ train_prior.sh
@@ -47,7 +47,7 @@ for ((i=$start; i<=$last; i++)); do
     # 学習
     python -m dlshogi.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
      ${resume} --checkpoint ${checkpoint} --network policy_value_network_densenet.PolicyValueNetwork --model ${model} -e 1\
-    --use_average --use_evalfix --use_swa --use_amp --temperature 0 --lr 0.2\
+    --use_average --use_evalfix --use_amp --temperature 0 --lr 0.2\
     --lr_scheduler ReduceLROnPlateau'('eps=1e-20,factor=0.5')' --log ${log_dir}/train_log.txt
 
     
