cb952ec299b06db3f5e261f06f129d3977ae836f
GenkiTakeuchi <genki.takeuchi@access-company.com>
2024-12-22 11:39:15 +0000

    モデルファイルのファイル名変更に対応

--- train_gct-model.sh
+++ train_gct-model.sh
@@ -57,7 +57,7 @@ for ((i=$start; i<=$last; i++)); do
 
     # 学習
     python -m dlshogi.train ${src} ${data_dir}/floodgate_test_2017-2018_r3500_eval5000.hcpe\
-     ${resume} --checkpoint ${checkpoint} --network policy_value_network_densenet.PolicyValueNetwork --model ${model} -e 1\
+     ${resume} --checkpoint ${checkpoint} --network policy_value_network_ponkotsu.PolicyValueNetwork --model ${model} -e 1\
     --use_average --use_evalfix --use_swa --use_amp --temperature 1 ${lr} --log ${log_dir}/train_log.txt
 
     
