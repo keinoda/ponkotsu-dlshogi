4b50831155b4e0600259fd6967913dea6246c5c4
jj1guj <jj1guj@jj1guj.net>
2025-08-20 21:14:59 +0900

    手動で追加した探索局面を自動で追加するスクリプトを作成

---
+++ books/visualize_search_board/add_sfen.py
@@ -0,0 +1,25 @@
+import argparse
+
+args = argparse.ArgumentParser()
+args.add_argument('sfens')
+args.add_argument('add_sfens')
+args = args.parse_args()
+
+with open(args.sfens, "r") as f:
+    sfens = f.readlines()
+
+if "sfen" not in sfens[-1]:
+    sfens = sfens[:-1]
+
+with open(args.add_sfens, "r") as f:
+    add_sfens = f.readlines()
+
+add_sfens = [f"sfen {s}" for s in add_sfens]
+
+sfens += add_sfens
+
+with open(args.sfens, "w") as f:
+    f.writelines(sfens)
+
+with open(args.add_sfens, "w") as f:
+    f.writelines([])
