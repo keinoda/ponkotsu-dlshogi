1cbaa8eb26849294ea503b2f96b5303a6538b10d
jj1guj <jj1guj@jj1guj.net>
2025-08-22 23:51:13 +0900

    実行ファイルのディレクトリと保存先のディレクトリを指定できるようにした

--- books/visualize_search_board/make_sfens_floodgate.sh
+++ books/visualize_search_board/make_sfens_floodgate.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+exec_dir=$1
+cd $2
+
 yesterday=$(date -d "yesterday" +%Y%m%d)
 
 wget http://wdoor.c.u-tokyo.ac.jp/shogi/x/kifuarchive-daily/wdoor$yesterday.7z
 7z x wdoor$yesterday.7z
 rm wdoor$yesterday.7z
 
-python csa_to_sfens.py $yesterday add.sfens --filter_score 300
+python $exec_dir/csa_to_sfens.py $yesterday add.sfens --filter_score 300
 rm -rf $yesterday
