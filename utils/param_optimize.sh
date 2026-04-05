efeb1763767dcdf0163aced3b5340b632218936d
jj1guj <jj1guj@jj1guj.net>
2026-04-05 16:06:22 +0900

    trialsを修正

--- utils/param_optimize.sh
+++ utils/param_optimize.sh
@@ -14,5 +14,5 @@ python -u -m dlshogi.utils.usi_params_optimizer --study_name model_resnet35x512_
 --options1 PV_Interval:0,PV_Mate_Search_Threads:3,DNN_Model:/root/models/model_resnet35x512-348.onnx,NetworkDelay2:0,MaxMovesToDraw:324 \
 --options2 PV_Interval:0,NetworkDelay2:0,Threads:2,USI_Hash:256,EvalDir:/volume/book/suisho11b,FV_SCALE:28,MaxMovesToDraw:324 \
 --suggest_params "C_init:100~170,C_base:20000~50000,C_fpu_reduction:0~25,C_init_root:100~175,C_base_root:18000~35000,Softmax_Temperature:160~300" \
---opening /volume/measure_strength/yaneuraou_start/start_sfens.txt --games 100 --trials 150 --byoyomi 2000 --csa /volume/param_optimize/kifu/resnet35x512_phase2 \
+--opening /volume/measure_strength/yaneuraou_start/start_sfens.txt --games 100 --trials 50 --byoyomi 2000 --csa /volume/param_optimize/kifu/resnet35x512_phase2 \
  | tee -a /volume/param_optimize/log_optimize_model_resnet35x512_phase2.txt
