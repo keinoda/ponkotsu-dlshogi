caf86aaf5d2228f96e49b8467b42a72a371a1aef
jj1guj <jj1guj@jj1guj.net>
2026-04-18 19:58:41 +0900

    add debug code

--- books/visualize_search_board/mcts_on_book_dl.py
+++ books/visualize_search_board/mcts_on_book_dl.py
@@ -286,6 +286,7 @@ if __name__ == "__main__":
     book_tree[board_key].child_move_count = np.zeros(len(book_tree[board_key].child_move))
     book_tree[board_key].child_score = np.array(book_tree[board_key].child_score, dtype=np.float32)
     book_tree[board_key].child_score_sum = np.zeros(len(book_tree[board_key].child_move), dtype=np.float32)
+    print(TEST_KEY in book_tree)
 
     # 反転が含まれていなければ追加する
     book_tree_rotated = dict()
@@ -302,6 +303,7 @@ if __name__ == "__main__":
             book_tree_rotated[rotated_board_key].child_score_sum = np.zeros(len(book_tree[key].child_move), dtype=np.float32)
 
     book_tree.update(book_tree_rotated)
+    print(TEST_KEY in book_tree)
 
     # DLで評価したノードを読み込む
     with open(args.dl_pickle, "rb") as f:
